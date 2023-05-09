80742115ac11015f6bf5c504d5fb1bd981f74a23
Amal GUITTOUM <32742497+Amalguittoum@users.noreply.github.com>
2023-05-09 11:28:02 +0200

    Create README.md

---
+++ QualitativeEvaluation-ISWC/README.r
@@ -0,0 +1,11 @@
+Folder for storing Qualitative Evaluation Materials proposed for the ISWC conference.
+The Qualitative Evaluation consists on Managing a set of casacding failure scenarios using five OSAMA agents.
+See sub-folders for specific content:
+```
+collaborativeDM-OSAMA-agent/QualitativeEvaluation-ISWC/
+├───The Smart Home Dependency Knowledge Graph
+└───The Failure Knowledge Base of the Kelvin OSAMA Agent
+└───The Failure Knowledge Base of the Phillips OSAMA Agent
+└───Cascading Failure Management Traces <representing message exchange between OSAMAs to solve the cascading failure scenarios>.
+
+```
